aec705c2c8581ed10b805cc96218e5a2e3ab8b5c
Kirill Akatov <kee15roll@gmail.com>
2016-05-08 23:45:41 +0300

    version 1.3.1
    I added 3 c# scripts:
    1)Player
    2)Recorder
    3)TimeController

---
+++ Time_Travel_Game/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] TimeController timeController;
+
+	Dictionary<int,PlayerState> recording;
+
+	//Animator _animator;
+
+	void Start ()
+	{
+		//_animator = GetComponent<Animator> ();
+
+	}
+
+	public void SetRecording (Dictionary<int, PlayerState> recording)
+	{
+		this.recording = new Dictionary<int, PlayerState> (recording);
+		isPlaying = true;
+
+	}
+
+	void Update()
+	{
+		
+	}
+
+
+	bool isPlaying = false;
+	void FixedUpdate ()
+	{
+		if (isPlaying) 
+		{
+			if (recording.ContainsKey (timeController.time))
+			{
+				PlayState(recording [timeController.time]);
+			}
+		}
+	}
+
+	void PlayState(PlayerState playerState)
+	{
+		transform.position = playerState.position;
+		//_animator.Play(playerState.animationStatement);
+		Vector3 localScale = transform.localScale;
+		localScale.x = playerState.direction ? Mathf.Abs (localScale.x) : -Mathf.Abs (localScale.x);
+	}
+}
--- Time_Travel_Game/Assets/Scripts/Recorder.cs
+++ Time_Travel_Game/Assets/Scripts/Recorder.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Recorder : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+public struct PlayerState
+{
+	public Vector3 position;
+	//public int animationStatement;
+	public bool direction;
+
+	public PlayerState(Vector3 position,/*int animationStatement*/ bool direction)
+	{
+		this.position = position;
+		//this.animationStatement = animationStatement;
+		this.direction = direction;
+	}
+}
+
+public class Recorder : MonoBehaviour 
+{
+
+	[SerializeField] TimeController timeController;
+	[SerializeField] Player player;
+
+	Dictionary<int,PlayerState> states = new Dictionary<int, PlayerState> ();
+
+	//Animator _animator;
+
+	void Start ()
+	{
+		//_animator = GetComponent<Animator> ();
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.P)) 
+		{
+			isRecording = false;
+			player.SetRecording(states);
+			timeController.time = 0;
+		}
+	}
+
+
+	bool isRecording = true;
+
+	void FixedUpdate () 
+	{
+		if (isRecording) 
+		{
+			states.Add (timeController.time, new PlayerState (transform.position,
+				/*_animator.GetCurrentAnimatorStateInfo(0).shortNameHash,*/ 
+				transform.localScale.x > 0));
+		}
+		Debug.Log ("states: " + states.Count);
 	
 	}
 }
