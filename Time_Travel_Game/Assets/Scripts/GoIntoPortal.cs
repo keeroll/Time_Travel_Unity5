92548f23689e9dfa846c46024e502b9754aeab96
Kirill Akatov <kee15roll@gmail.com>
2016-05-09 02:40:06 +0300

    v 1.4
    Added portals
    Improvement needed

---
+++ Time_Travel_Game/Assets/Scripts/GoIntoPortal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoIntoPortal : MonoBehaviour {
+
+	public GameObject anotherPortal;
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		if (otherCollider.tag == "Player") 
+		{
+			otherCollider.transform.position = anotherPortal.transform.position;
+		}
+
+	}
+}
