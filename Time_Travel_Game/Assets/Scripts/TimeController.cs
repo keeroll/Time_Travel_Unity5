b05155a2d4846e74830a24e7fb75a9e90718f3d1
Kirill Akatov <kee15roll@gmail.com>
2016-05-08 22:45:00 +0300

    version 1.1
    Little road system, default FPS controller, Ghost house (model)

---
+++ Time_Travel_Game/Assets/Scripts/TimeController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour {
+
+	public int time;
+	bool isForward = true;
+
+	void FixedUpdate () {
+		if (isForward) 
+		{
+			time++;
+		}
+	}
+}
