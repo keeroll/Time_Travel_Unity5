9ba3095153b31c3574751fc09d758f6983d8c60e
Kirill Akatov <kee15roll@gmail.com>
2016-05-09 19:31:21 +0300

    v 1.4.4
    Blocked exit in tunnel
    Done with particle system in portal

---
+++ Time_Travel_Game/Assets/Scripts/MoveRoundPortal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRoundPortal : MonoBehaviour {
+
+	public GameObject portal;
+
+
+	void Update () {
+	
+		transform.RotateAround(portal.transform.position, Vector3.down, Time.deltaTime * 8);
+	}
+}
